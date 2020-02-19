13e0f4ab83585d4dc0583c0b18c7b365f6dffe63
huangting <tingh5842@163.com>
2020-02-19 17:39:40 +0800

    delete sth

--- merchantCenter/config/dev.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  NODE_ENV: '"development"',
-  ENV_CONFIG: '"dev"',
-  // BASE_API: '"https://biztriptest.youpinsanyue.com/tripManager"'
-  BASE_API: '"https://test-ctrip-manager-api.youpinsanyue.com"'
-  // BASE_API: '"https://api-dev"'
-}
